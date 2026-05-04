c4b422593522a212379fae13d143ff05513acc1f
Naivell453 <eduardovb.453@gmail.com>
2026-05-04 16:17:21 -0500

    Create main.c

---
+++ firmware/main.c
@@ -0,0 +1,17 @@
+/*
+ * Stasis Console Firmware
+ * Built with Buildroot for Orange Pi Zero 2W (Allwinner H618, ARM64)
+ * 
+ * Build system: Buildroot
+ * Target: Orange Pi Zero 3 defconfig (compatible with Zero 2W)
+ * Autorun: /etc/init.d/S99game
+ * Game engine: Godot 4.6
+ * 
+ * To build:
+ * make orangepi_zero3_defconfig
+ * make -j1
+ */
+
+int main() {
+    return 0;
+}
